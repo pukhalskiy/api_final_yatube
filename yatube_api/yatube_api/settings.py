7694653768c154d0ef06852770ca392cf380687c
perineum14 <ego3200@gmail.com>
2023-05-26 21:29:52 +0300

    pep8 improvments

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -97,13 +97,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],  
+    ],
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
-} 
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',)}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
